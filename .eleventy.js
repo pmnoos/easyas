f4d65555188b2512696f0cd8b1991ee603e92e83
pmnoos <petermagner3@gmail.com>
2022-03-15 10:28:13 +0000

    added jpg

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
